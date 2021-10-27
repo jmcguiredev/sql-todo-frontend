bdceca99f72b658384ca182850316b262aacc920
Jacob McGuire <dev.jmcguire@gmail.com>
2021-10-27 15:13:55 -0400

    test data, basic list and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
-import ReactLogo from './assets/reactlogo.svg';
+import React from 'react';
+import todoData from './testData.json';
+
+let { todos } = todoData;
 
 
 const App = ({}) => {
     return (
         <div id="app">
-            <div className="intro-container">
-                <h1 className="intro intro-heading">Welcome to your React Electron App</h1>
-                <img className="app-icon" src={ReactLogo}/>
+            <div className="todo-container">
+                <div className="todo-header">
+                    <span className="todo-cell todo-header-id">ID</span>
+                    <span className="todo-cell todo-header-label">Label</span>
+                    <span className="todo-cell todo-header-note">Note</span>
+                    <span className="todo-cell todo-header-completed">Complete</span>
+                </div>
+                {todos.forEach((todo) => (
+                    <div className="todo-row">
+                        <span className="todo-cell todo-entry todo-entry-id"></span>
+                        <span className="todo-cell todo-entry todo-entry-label"></span>
+                        <span className="todo-cell todo-entry todo-entry-note"></span>
+                        <span className="todo-cell todo-entry todo-entry-completed"></span>
+                    </div>
+                ))}
             </div>
         </div>
     );
