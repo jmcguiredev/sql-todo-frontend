ddeda5e76cf278812f48a5a300f7030e3b034bb9
Jacob McGuire <dev.jmcguire@gmail.com>
2021-10-29 15:31:41 -0400

    fixed CORS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import todoData from './testData.json';
+import React, { useEffect, useState } from 'react';
+const axios = require('axios');
 
-let { todos } = todoData;
+let todoArr;
+
+
 
 
 const App = ({}) => {
+    const [todos, setTodos] = useState();
+
+    async function getTodos() {
+        axios.get('http://localhost:3000/todos').then(res => {
+            console.log(res);
+            todoArr = [...res.data];
+            setTodos(todoArr);
+          })
+        
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, []); // the second argument [] makes this only run on mount and not re-renders
+
+    
+
     return (
         <div id="app">
             <div className="todo-container">
@@ -14,14 +33,14 @@ const App = ({}) => {
                     <span className="todo-cell todo-header-note">Note</span>
                     <span className="todo-cell todo-header-completed">Complete</span>
                 </div>
-                {todos.map((todo) => (
-                    <div className="todo-row">
-                        <span className="todo-cell todo-entry todo-entry-id">{todo.id}</span>
+                {todos ? todos.map((todo) => (
+                    <div className="todo-row" key={todo._id}>
+                        <span className="todo-cell todo-entry todo-entry-id">{todo._id}</span>
                         <span className="todo-cell todo-entry todo-entry-label">{todo.label}</span>
                         <span className="todo-cell todo-entry todo-entry-note">{todo.note}</span>
                         <span className="todo-cell todo-entry todo-entry-completed">{todo.completed}</span>
                     </div>
-                ))}
+                )) : null}
             </div>
         </div>
     );
