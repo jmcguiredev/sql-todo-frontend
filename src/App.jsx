8a15d0fce50208c191cce7526cd159d444587d3b
Jacob McGuire <dev.jmcguire@gmail.com>
2021-10-30 14:01:35 -0400

    extracted TodoList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import TodoList from './components/TodoList/TodoList';
 const axios = require('axios');
 
 let todoArr;
@@ -26,22 +27,7 @@ const App = ({}) => {
 
     return (
         <div id="app">
-            <div className="todo-container">
-                <div className="todo-row">
-                    <span className="todo-cell todo-header-id">ID</span>
-                    <span className="todo-cell todo-header-label">Label</span>
-                    <span className="todo-cell todo-header-note">Note</span>
-                    <span className="todo-cell todo-header-completed">Complete</span>
-                </div>
-                {todos ? todos.map((todo) => (
-                    <div className="todo-row" key={todo._id}>
-                        <span className="todo-cell todo-entry todo-entry-id">{todo._id}</span>
-                        <span className="todo-cell todo-entry todo-entry-label">{todo.label}</span>
-                        <span className="todo-cell todo-entry todo-entry-note">{todo.note}</span>
-                        <span className="todo-cell todo-entry todo-entry-completed">{todo.completed}</span>
-                    </div>
-                )) : null}
-            </div>
+            <TodoList todos={todos}/>
         </div>
     );
 }
